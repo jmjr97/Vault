86d3af441f81986f47a1645cfb8db1df2562fe9c
John Majka Jr <jmjr97@gmail.com>
2026-09-12 22:01:16 -0400

    updated yazi config, moved some scripts

---
+++ scripts/setup/set-fstab.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "UUID=cafe3915-0b73-4b2e-a583-795d6b9a9ee0 /run/media/Games ext4 defaults 0 0" >> /etc/fstab
+echo "UUID=d5356b63-688c-4d0e-ba75-bdd403958f14 /run/media/Storage ext4 defaults 0 0" >> /etc/fstab
